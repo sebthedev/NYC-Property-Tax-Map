13c5e3667bb886b0dc5e179233b8ea18a509ebed
Sebastian Hallum Clarke <s@sebthedev.com>
2023-11-10 12:22:53 -0500

    Optimized query performance

--- dataJoin.sql
+++ dataJoin.sql
@@ -7,26 +7,25 @@ WITH
     ST_GEOGFROMTEXT(the_geom) AS AssemblyDistrictGeometry
   FROM
     `sidewalk-chorus.propertytaxmap.NYC_Assembly_Districts`),
-  get_tax_rate AS ( -- Synthetic table for the tax rates
+  tax_rates_table AS ( -- Synthetic table for the tax rates
   SELECT
-    1 AS tax_class,
-    20.31 AS tax_rate
+    1 AS TaxClass,
+    20.31 / 100 AS TaxRate
   UNION ALL
   SELECT
     2,
-    12.27
+    12.27 / 100
   UNION ALL
   SELECT
     3,
-    12.76
+    12.76 / 100
   UNION ALL
   SELECT
     4,
-    10.65 ),
-  abatements AS ( -- Aggregation of abatements for each property
+    10.65 / 100 ),
+  abatements_computed AS ( -- Aggregation of abatements for each property
   SELECT
-    parid,
-    COUNT(*) AS DistinctAbatements,
+    CAST(parid AS STRING) AS BoroughBlockLot,
     SUM(appliedabt) AS SumOfAppliedAbatements
   FROM
     `sidewalk-chorus.propertytaxmap.Abatements`
@@ -34,103 +33,84 @@ WITH
     period = "2Q"
   GROUP BY
     parid),
-  precomputed_data AS (
+  pvad_computed AS (
   SELECT
-  IF
-    (pvad.condo_number IS NOT NULL, CAST(pvad.condo_number AS STRING), pvad.parid) AS PropertyGroupKey,
-    pvad.parid AS BoroughBlockLot,
+    CAST(pvad.parid AS STRING) AS BoroughBlockLot,
+    CAST(pvad.condo_number AS STRING) AS CondoNumber,
+    CAST(LEFT(pvad.curtaxclass, 1) AS INT64) AS TaxClass,
+    pvad.bldg_class AS BuildingClass,
+    (pvad.condo_number IS NOT NULL
+      AND pvad.bldg_class != "R0") AS IsCondoUnit,
+    pvad.curmkttot AS CurrentMarketTotalValue,
+    pvad.curtxbtot AS CurrentTaxableValue,
+    pvad.curtxbextot AS CurrentExemptionValue
+  FROM
+    `sidewalk-chorus.propertytaxmap.PVAD_2023-11-10_3` AS pvad),
+  pluto_computed AS (
+  SELECT
+    CAST(pluto.bbl AS STRING) AS BoroughBlockLot,
     pluto.address AS Address,
     pluto.zipcode AS ZipCode,
     pluto.borough AS Borough,
     pluto.ownername AS OwnerName,
-    -- If the property is a condo unit, determine the name of the unit owner (in addition to the name of the building owner, which is typically the condo association)
-  IF
-    (pvad.condo_number IS NOT NULL
-      AND pvad.bldg_class != "R0", pvad.owner, NULL) AS CondoUnitOwnerName,
-    pvad.condo_number AS CondoNumber,
     pluto.council AS CouncilDistrict,
     pluto.cd AS CommunityDistrict,
-    assembly_districts.AssemblyDistrictNumber AS AssemblyDistrict,
     pluto.latitude AS Latitude,
     pluto.longitude AS Longitude,
+    ST_GEOGPOINT(pluto.longitude, pluto.latitude) AS GeoGPoint,
     pluto.yearbuilt AS YearBuilt,
-  IF
-    (pvad.condo_number IS NOT NULL
-      AND pvad.bldg_class != "R0", 1, pluto.unitsres) AS ResidentialUnits,
-    pvad.curtaxclass AS TaxClass,
-  IF
-    (pvad.condo_number IS NOT NULL
-      AND pvad.bldg_class != "R0", TRUE, FALSE) AS IsCondoUnit,
-    pvad.curmkttot AS CurrentMarketTotalValue,
-    pvad.curtxbtot AS CurrentTaxableValue,
-    pvad.curtxbextot AS CurrentExemptionValue,
-    abatements.DistinctAbatements,
-    abatements.SumOfAppliedAbatements,
-    ROUND((pvad.curtxbtot - pvad.curtxbextot) * tax_rate_table.tax_rate / 100 - IFNULL(abatements.SumOfAppliedAbatements, 0), 2) AS TaxBill,
-    ROUND(SAFE_DIVIDE((pvad.curtxbtot - pvad.curtxbextot) * tax_rate_table.tax_rate / 100 - IFNULL(abatements.SumOfAppliedAbatements, 0), pvad.curmkttot),8) AS EffectiveTaxRate,
+    pluto.unitsres AS ResidentialUnits,
+    CONCAT(LEFT(CAST(pluto.bbl AS STRING), 1), FORMAT('%05d', pluto.condono)) AS CondoNumber -- PLUTO has a per-borough condo number field. We need to combine it with the borocode (first digit of BBL to determine the property's cross-NYC condo number)
   FROM
-    `sidewalk-chorus.propertytaxmap.PVAD_2023-11-10_3` AS pvad
-  LEFT JOIN
-    `sidewalk-chorus.propertytaxmap.PLUTO_2023-11-10` AS pluto
-  ON
-    -- Join on the BBL if this property is not a condo. Join on the condo_number if this property is a condo
-  IF
-    (pvad.condo_number IS NOT NULL, CAST(pvad.condo_number AS STRING) = CONCAT(LEFT(CAST(pluto.bbl AS STRING), 1), FORMAT('%05d', pluto.condono)), CAST(pluto.bbl AS STRING) = pvad.parid)
-  JOIN
-    get_tax_rate AS tax_rate_table
-  ON
-    pvad.curtaxclass = CAST(tax_rate_table.tax_class AS STRING)
-  LEFT JOIN
-    abatements
-  ON
-    pvad.parid = CAST(abatements.parid AS STRING)
-    -- Join with Assembly District boundaries
-  LEFT JOIN
-    assembly_districts
-  ON
-    ST_CONTAINS(assembly_districts.AssemblyDistrictGeometry, ST_GEOGPOINT(pluto.longitude, pluto.latitude))
-    -- WHERE
-    --   pvad.condo_number = 100058
-    --   OR pvad.parid = "1013960033"
-    --   OR pvad.parid = "1013940023"
-    )
+    `sidewalk-chorus.propertytaxmap.PLUTO_2023-11-10` AS pluto)
 SELECT
-  -- Now group the data together. This allows us to sum up the values and tax bills for condos, which are represented as separate tax units
+IF
+  (pvad_computed.CondoNumber IS NOT NULL, pvad_computed.CondoNumber, pvad_computed.BoroughBlockLot) AS PropertyGroupKey,
   MAX(CASE
-      WHEN IsCondoUnit = FALSE THEN BoroughBlockLot
+      WHEN pvad_computed.IsCondoUnit = FALSE THEN pvad_computed.BoroughBlockLot
     ELSE
     NULL
   END
     ) AS BoroughBlockLot,
-  ARRAY_AGG(BoroughBlockLot
+  ARRAY_AGG(pvad_computed.BoroughBlockLot
   ORDER BY
-    BoroughBlockLot ASC) AS ComponentPropertyBoroughBlockLots,
-  ANY_VALUE(CondoNumber) AS CondoNumber,
-  ANY_VALUE(AssemblyDistrict) AS AssemblyDistrict,
+    pvad_computed.BoroughBlockLot ASC) AS ComponentPropertyBoroughBlockLots,
+  ANY_VALUE(pvad_computed.CondoNumber) AS CondoNumber,
+  ANY_VALUE(assembly_districts.AssemblyDistrictNumber) AS AssemblyDistrict,
   ANY_VALUE(CouncilDistrict) AS CouncilDistrict,
   ANY_VALUE(CommunityDistrict) AS CommunityDistrict,
   ANY_VALUE(Latitude) AS Latitude,
   ANY_VALUE(Longitude) AS Longitude,
   ANY_VALUE(YearBuilt) AS YearBuilt,
-  MAX(CASE
-      WHEN IsCondoUnit = FALSE THEN ResidentialUnits
-    ELSE
-    NULL
-  END
-    ) AS ResidentialUnits,
-  MAX(CASE
-      WHEN IsCondoUnit = FALSE THEN TaxClass
-    ELSE
-    NULL
-  END
-    ) AS TaxClass,
-  SUM(CurrentMarketTotalValue) AS CurrentMarketTotalValue,
-  SUM(CurrentTaxableValue) AS CurrentTaxableValue,
-  SUM(CurrentExemptionValue) AS CurrentExemptionValue,
-  SUM(SumOfAppliedAbatements) AS SumOfAppliedAbatements,
-  SUM(TaxBill) AS TaxBill,
-  ROUND(SAFE_DIVIDE(SUM(TaxBill), SUM(CurrentMarketTotalValue)), 8) AS EffectiveTaxRate
+  ANY_VALUE(pvad_computed.TaxClass) AS TaxClass,
+  ANY_VALUE(pvad_computed.CondoNumber IS NOT NULL) AS IsCondoProperty,
+  SUM(pvad_computed.CurrentMarketTotalValue) AS CurrentMarketTotalValue,
+  SUM(pvad_computed.CurrentTaxableValue) AS CurrentTaxableValue,
+  SUM(pvad_computed.CurrentExemptionValue) AS CurrentExemptionValue,
+  SUM(abatements_computed.SumOfAppliedAbatements) AS SumOfAppliedAbatements,
+  ROUND(SUM((pvad_computed.CurrentTaxableValue - pvad_computed.CurrentExemptionValue) * tax_rates_table.TaxRate - IFNULL(abatements_computed.SumOfAppliedAbatements, 0)), 2) AS TaxBill,
+  ROUND(SAFE_DIVIDE(SUM((pvad_computed.CurrentTaxableValue - pvad_computed.CurrentExemptionValue) * tax_rates_table.TaxRate - IFNULL(abatements_computed.SumOfAppliedAbatements, 0)), SUM(pvad_computed.CurrentMarketTotalValue)),8) AS EffectiveTaxRate,
 FROM
-  precomputed_data
+  pvad_computed
+INNER JOIN -- only keep the properties that are in both PVAD and PLUTO. There are some properties that (for some reason) are not included in PLUTO. This is mostly class 4 utility properties.
+  pluto_computed
+ON
+  pvad_computed.BoroughBlockLot = pluto_computed.BoroughBlockLot
+LEFT JOIN
+  assembly_districts
+ON
+  ST_CONTAINS(assembly_districts.AssemblyDistrictGeometry, pluto_computed.GeoGPoint)
+LEFT JOIN
+  abatements_computed
+ON
+  pvad_computed.BoroughBlockLot = abatements_computed.BoroughBlockLot
+LEFT JOIN
+  tax_rates_table
+ON
+  pvad_computed.TaxClass = tax_rates_table.TaxClass
+  -- WHERE
+  --   pvad_computed.CondoNumber = "100058"
+  --   OR pvad_computed.BoroughBlockLot IN ("1013960033",
+  --     "1013940023")
 GROUP BY
-  PropertyGroupKey;
+  PropertyGroupKey ;
